d629492cc42d7824947a872004f9d7edc8fa83d7
basedantoni <anthony.mercado300@gmail.com>
2025-07-10 12:23:04 -0500

    add home button to confirmation page

--- src/routes/confirmation.tsx
+++ src/routes/confirmation.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { useTranslation } from 'react-i18next';
 import { useFormStepper } from '@/hooks/form';
-import { ExternalLink } from 'lucide-react';
+import { ArrowLeftIcon, ExternalLink } from 'lucide-react';
+import { useEffect } from 'react';
 
 export const Route = createFileRoute('/confirmation')({
   component: RouteComponent,
@@ -10,52 +11,65 @@ export const Route = createFileRoute('/confirmation')({
 function RouteComponent() {
   const { t } = useTranslation();
 
-  const { childrenVerification } = useFormStepper();
+  const { childrenVerification, setCurrentStep } = useFormStepper();
+
+  useEffect(() => {
+    setCurrentStep(0);
+  }, []);
 
   return (
-    <div className='flex flex-col items-center space-y-2 justify-center h-screen'>
-      <Link to='/'>
-        <img src='/img/zachry-logo.webp' alt='Zachry Logo' />
-      </Link>
-      <h1 className='text-2xl font-bold text-center'>
-        {t('thankYouForCompletingTheZachryPicnicForm')}
-      </h1>
-
-      <p className='text-sm'>{t('correctionsOrRevisions')}</p>
-      {childrenVerification && (
-        <p className='text-sm text-destructive'>
-          {t('dependentChildrenVerification')}
-        </p>
-      )}
-      <div className='flex flex-col items-center space-y-2 mt-4'>
-        <h2 className='text-2xl'>Hotel Information</h2>
-        <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
-          <a
-            href='/embassy-airport.pdf'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='underline cursor-pointer'
-          >
-            Embassy Suites by Hilton San Antonio Airport
-          </a>
-          <ExternalLink className='w-4 h-4' />
-        </div>
-
-        <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
-          <a
-            href='/embassy-landmark.pdf'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='underline cursor-pointer'
-          >
-            Embassy Suites by Hilton San Antonio Landmark
-          </a>
-          <ExternalLink className='w-4 h-4' />
-        </div>
+    // Header with logo and back button
+    <>
+      <div className='flex items-center justify-between w-full p-3 sm:px-4 sm:py-4'>
+        <Link to='/' className='flex items-center gap-2'>
+          <ArrowLeftIcon className='w-4 h-4' />
+          {t('backToHome')}
+        </Link>
       </div>
-      <h3 className='text-lg text-muted-foreground mt-4'>
-        {t('orderConfirmationDescription')}
-      </h3>
-    </div>
+      <div className='flex flex-col items-center space-y-2 justify-center h-screen'>
+        <Link to='/'>
+          <img src='/img/zachry-logo.webp' alt='Zachry Logo' />
+        </Link>
+        <h1 className='text-2xl font-bold text-center'>
+          {t('thankYouForCompletingTheZachryPicnicForm')}
+        </h1>
+
+        <p className='text-sm'>{t('correctionsOrRevisions')}</p>
+        {childrenVerification && (
+          <p className='text-sm text-destructive'>
+            {t('dependentChildrenVerification')}
+          </p>
+        )}
+        <div className='flex flex-col items-center space-y-2 mt-4'>
+          <h2 className='text-2xl'>Hotel Information</h2>
+          <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+            <a
+              href='/embassy-airport.pdf'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='underline cursor-pointer'
+            >
+              Embassy Suites by Hilton San Antonio Airport
+            </a>
+            <ExternalLink className='w-4 h-4' />
+          </div>
+
+          <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+            <a
+              href='/embassy-landmark.pdf'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='underline cursor-pointer'
+            >
+              Embassy Suites by Hilton San Antonio Landmark
+            </a>
+            <ExternalLink className='w-4 h-4' />
+          </div>
+        </div>
+        <h3 className='text-lg text-muted-foreground mt-4'>
+          {t('orderConfirmationDescription')}
+        </h3>
+      </div>
+    </>
   );
 }
