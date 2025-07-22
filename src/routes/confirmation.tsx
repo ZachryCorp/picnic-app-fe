6bfe92b88941d10250633b7aba055f5912d7aece
basedantoni <anthony.mercado300@gmail.com>
2025-07-22 15:41:27 -0500

    only show park specific hotel information on confirmation page

--- src/routes/confirmation.tsx
+++ src/routes/confirmation.tsx
@@ -11,7 +11,7 @@ export const Route = createFileRoute('/confirmation')({
 function RouteComponent() {
   const { t } = useTranslation();
 
-  const { childrenVerification, setCurrentStep } = useFormStepper();
+  const { childrenVerification, setCurrentStep, park } = useFormStepper();
 
   useEffect(() => {
     setCurrentStep(0);
@@ -40,32 +40,59 @@ function RouteComponent() {
             {t('dependentChildrenVerification')}
           </p>
         )}
-        <div className='flex flex-col items-center space-y-2 mt-4'>
-          <h2 className='text-2xl'>Hotel Information</h2>
-          <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
-            <a
-              href='/embassy-airport.pdf'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='underline cursor-pointer'
-            >
-              Embassy Suites by Hilton San Antonio Airport
-            </a>
-            <ExternalLink className='w-4 h-4' />
-          </div>
+        {park === 'Fiesta Texas' && (
+          <div className='flex flex-col items-center space-y-2 mt-4'>
+            <h2 className='text-2xl'>Hotel Information</h2>
+            <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+              <a
+                href='/embassy-airport.pdf'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='underline cursor-pointer'
+              >
+                Embassy Suites by Hilton San Antonio Airport
+              </a>
+              <ExternalLink className='w-4 h-4' />
+            </div>
 
-          <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
-            <a
-              href='/embassy-landmark.pdf'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='underline cursor-pointer'
-            >
-              Embassy Suites by Hilton San Antonio Landmark
-            </a>
-            <ExternalLink className='w-4 h-4' />
+            <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+              <a
+                href='/embassy-landmark.pdf'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='underline cursor-pointer'
+              >
+                Embassy Suites by Hilton San Antonio Landmark
+              </a>
+              <ExternalLink className='w-4 h-4' />
+            </div>
           </div>
-        </div>
+        )}
+        {park === 'Carowinds' && (
+          <div className='flex flex-col items-start space-y-2 mt-4'>
+            <p className='text-sm font-bold'>Zachry Construction 2025</p>
+            <p className='text-sm'>Check in Date: Friday, September 26, 2025</p>
+            <p className='text-sm'>
+              Check out Date: Sunday, September 27, 2025
+            </p>
+            <p className='text-sm'>Hotel offering your special group rate:</p>
+            <p className='text-sm'>
+              TownPlace Suites Fort Mill at Carowinds Blvd. for 119.00 USD per
+              night - Last Day to Book: Friday, September 12, 2025
+            </p>
+            <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+              <a
+                href='https://www.marriott.com/event-reservations/reservation-link.mi?id=1752866477439&key=GRP&app=resvlink'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='underline cursor-pointer'
+              >
+                Book your group rate for Zachry Construction 2025
+              </a>
+              <ExternalLink className='w-4 h-4' />
+            </div>
+          </div>
+        )}
         <h3 className='text-lg text-muted-foreground mt-4'>
           {t('orderConfirmationDescription')}
         </h3>
