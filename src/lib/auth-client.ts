4b8d609ed6cf98f06ac317f1af3bc42d1b63b7d2
basedantoni <anthony.mercado300@gmail.com>
2025-07-24 08:22:59 -0500

    add better-auth

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,5 @@
+import { createAuthClient } from "better-auth/react";
+export const { signIn, signUp, signOut } = createAuthClient({
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+});
--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,14 +1,14 @@
-import { SubmissionTableWrapper } from '@/components/submissions/submission-table-wrapper';
-import { createFileRoute } from '@tanstack/react-router';
+import { SubmissionTableWrapper } from "@/components/submissions/submission-table-wrapper";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/admin')({
+export const Route = createFileRoute("/admin")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
-    <main className='flex flex-col py-8 container mx-auto justify-center space-y-8'>
-      <h1 className='text-2xl font-bold'>Admin</h1>
+    <main className="flex flex-col py-8 container mx-auto justify-center space-y-8">
+      <h1 className="text-2xl font-bold">Admin</h1>
       <SubmissionTableWrapper />
     </main>
   );
