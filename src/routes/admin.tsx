3858f0e4c9d4c8865335038b3b6150035a7781a4
basedantoni <anthony.mercado300@gmail.com>
2025-07-24 11:47:00 -0500

    add admin login

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,15 +1,32 @@
 import { SubmissionTableWrapper } from "@/components/submissions/submission-table-wrapper";
+import { SignIn } from "@/components/auth/sign-in";
 import { createFileRoute } from "@tanstack/react-router";
+import { getSession, useSession } from "@/lib/auth-client";
 
 export const Route = createFileRoute("/admin")({
   component: RouteComponent,
+  loader: async () => {
+    const { data: session } = await getSession();
+    if (!session?.user) {
+      return {
+        user: null,
+      };
+    } else {
+      return {
+        user: session.user,
+      };
+    }
+  },
 });
 
 function RouteComponent() {
+  const { data: session } = useSession();
+
   return (
     <main className="flex flex-col py-8 container mx-auto justify-center space-y-8">
       <h1 className="text-2xl font-bold">Admin</h1>
-      <SubmissionTableWrapper />
+
+      {!session?.user ? <SignIn /> : <SubmissionTableWrapper />}
     </main>
   );
 }
