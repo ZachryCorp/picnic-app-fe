ac4b8dcefdf29e95daec3f655de0641f68cca124
Anthony Mercado <antho@Slabtop-Air.local>
2025-08-12 09:40:22 -0700

    change external link for carowinds hotel

--- src/routes/confirmation.tsx
+++ src/routes/confirmation.tsx
@@ -1,10 +1,10 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
-import { useTranslation } from 'react-i18next';
-import { useFormStepper } from '@/hooks/form';
-import { ArrowLeftIcon, ExternalLink } from 'lucide-react';
-import { useEffect } from 'react';
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
+import { useFormStepper } from "@/hooks/form";
+import { ArrowLeftIcon, ExternalLink } from "lucide-react";
+import { useEffect } from "react";
 
-export const Route = createFileRoute('/confirmation')({
+export const Route = createFileRoute("/confirmation")({
   component: RouteComponent,
 });
 
@@ -20,87 +20,87 @@ function RouteComponent() {
   return (
     // Header with logo and back button
     <>
-      <div className='flex items-center justify-between w-full p-3 sm:px-4 sm:py-6'>
-        <Link to='/' className='flex items-center gap-2'>
-          <ArrowLeftIcon className='w-4 h-4' />
-          {t('backToHome')}
+      <div className="flex items-center justify-between w-full p-3 sm:px-4 sm:py-6">
+        <Link to="/" className="flex items-center gap-2">
+          <ArrowLeftIcon className="w-4 h-4" />
+          {t("backToHome")}
         </Link>
       </div>
-      <div className='flex flex-col items-center space-y-2 justify-center h-screen'>
-        <Link to='/'>
-          <img src='/img/zachry-logo.webp' alt='Zachry Logo' />
+      <div className="flex flex-col items-center space-y-2 justify-center h-screen">
+        <Link to="/">
+          <img src="/img/zachry-logo.webp" alt="Zachry Logo" />
         </Link>
-        <h1 className='text-2xl font-bold text-center'>
-          {t('thankYouForCompletingTheZachryPicnicForm')}
+        <h1 className="text-2xl font-bold text-center">
+          {t("thankYouForCompletingTheZachryPicnicForm")}
         </h1>
 
-        <p className='text-sm'>{t('correctionsOrRevisions')}</p>
+        <p className="text-sm">{t("correctionsOrRevisions")}</p>
         {childrenVerification && (
-          <p className='text-sm text-destructive'>
-            {t('dependentChildrenVerification')}
+          <p className="text-sm text-destructive">
+            {t("dependentChildrenVerification")}
           </p>
         )}
-        {park === 'Fiesta Texas' && (
-          <div className='flex flex-col items-center space-y-2 mt-4'>
-            <h2 className='text-2xl'>Hotel Information</h2>
-            <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+        {park === "Fiesta Texas" && (
+          <div className="flex flex-col items-center space-y-2 mt-4">
+            <h2 className="text-2xl">Hotel Information</h2>
+            <div className="flex items-center gap-2 text-blue-600 visited:text-purple-800">
               <a
-                href='/embassy-airport.pdf'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='underline cursor-pointer'
+                href="/embassy-airport.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline cursor-pointer"
               >
                 Embassy Suites by Hilton San Antonio Airport
               </a>
-              <ExternalLink className='w-4 h-4' />
+              <ExternalLink className="w-4 h-4" />
             </div>
 
-            <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+            <div className="flex items-center gap-2 text-blue-600 visited:text-purple-800">
               <a
-                href='/embassy-landmark.pdf'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='underline cursor-pointer'
+                href="/embassy-landmark.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline cursor-pointer"
               >
                 Embassy Suites by Hilton San Antonio Landmark
               </a>
-              <ExternalLink className='w-4 h-4' />
+              <ExternalLink className="w-4 h-4" />
             </div>
           </div>
         )}
-        {park === 'Carowinds' && (
-          <div className='flex flex-col items-start space-y-2 mt-4'>
-            <p className='text-sm font-bold'>Zachry Construction 2025</p>
-            <p className='text-sm'>Check in Date: Friday, September 26, 2025</p>
-            <p className='text-sm'>
+        {park === "Carowinds" && (
+          <div className="flex flex-col items-start space-y-2 mt-4">
+            <p className="text-sm font-bold">Zachry Construction 2025</p>
+            <p className="text-sm">Check in Date: Friday, September 26, 2025</p>
+            <p className="text-sm">
               Check out Date: Sunday, September 27, 2025
             </p>
-            <p className='text-sm'>Hotel offering your special group rate:</p>
-            <p className='text-sm'>
-              Hilton Garden Inn Charlotte/Ayrsley for 134.00 USD per
-              night - Last Day to Book: Friday, September 12, 2025
+            <p className="text-sm">Hotel offering your special group rate:</p>
+            <p className="text-sm">
+              Hilton Garden Inn Charlotte/Ayrsley for 134.00 USD per night -
+              Last Day to Book: Friday, September 12, 2025
             </p>
-            <div className='flex items-center gap-2 text-blue-600 visited:text-purple-800'>
+            <div className="flex items-center gap-2 text-blue-600 visited:text-purple-800">
               <a
-                href='https://group.hiltongardeninn.com/xxp6qd'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='underline cursor-pointer'
+                href="https://group.hiltongardeninn.com/xxp6qd"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline cursor-pointer"
               >
-                Book your group rate for Zachry Construction 2025
+                Hilton Garden Inn Charlotte/Ayrsley
               </a>
-              <ExternalLink className='w-4 h-4' />
+              <ExternalLink className="w-4 h-4" />
             </div>
           </div>
         )}
-        <h3 className='text-lg text-muted-foreground mt-4'>
-          {t('orderConfirmationDescription')}
+        <h3 className="text-lg text-muted-foreground mt-4">
+          {t("orderConfirmationDescription")}
         </h3>
       </div>
-      <div className='flex items-center justify-between w-full p-3 sm:px-4 sm:py-6'>
-        <Link to='/' className='flex items-center gap-2'>
-          <ArrowLeftIcon className='w-4 h-4' />
-          {t('backToHome')}
+      <div className="flex items-center justify-between w-full p-3 sm:px-4 sm:py-6">
+        <Link to="/" className="flex items-center gap-2">
+          <ArrowLeftIcon className="w-4 h-4" />
+          {t("backToHome")}
         </Link>
       </div>
     </>
